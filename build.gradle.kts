09a561c628b3637a45cd5e4de4b8ada8295881de
Rafael Perondi <rafael.perondi@aluno.fmpsc.edu.br>
2025-11-26 08:58:39 -0300

    adicionar comentario

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    // A linha do Google Services (Firebase)
+    // A linha do Google Services
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
