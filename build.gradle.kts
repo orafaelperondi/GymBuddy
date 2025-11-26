5210dcd33c5a244c0550924f62882b997ec241fd
Rafael Perondi <rafael.perondi@aluno.fmpsc.edu.br>
2025-11-26 09:19:00 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add configuration options common to all sub-project
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    // A linha do Google
+    // A linha d
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
