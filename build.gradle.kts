748d99aca31289a1d8b116ad846f65da3b71a74b
Rafael Perondi <rafael.perondi@aluno.fmpsc.edu.br>
2025-11-26 09:16:01 -0300

    teste

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    // A linha do Google Services
+    // A linha do Google
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
